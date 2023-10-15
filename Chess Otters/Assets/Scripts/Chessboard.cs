f2ca90058c284ba4a28d612a485a71ae37739c64
Yiroh <69987911+Yiroh@users.noreply.github.com>
2023-10-14 22:18:53 -0400

    Chessboard Graphics
    Modeled and Imported a chessboard from blender, reimported it once to fix materials, then added functionality to the code to utilize the placement of the chessboard and still highlight what tile is being hovered.

--- Chess Otters/Assets/Scripts/Chessboard.cs
+++ Chess Otters/Assets/Scripts/Chessboard.cs
@@ -6,6 +6,8 @@ public class Chessboard : MonoBehaviour
     [SerializeField] private Material tileMaterial;
     [SerializeField] private Material hoverMaterial;
     [SerializeField] private float tileSize = 1.0f;
+    [SerializeField] private float yOffset = 0.2f;
+    [SerializeField] private Vector3 boardCenter = Vector3.zero;
 
     // LOGIC
     private const int tileCountX = 8;
@@ -13,6 +15,7 @@ public class Chessboard : MonoBehaviour
     private GameObject[,] tiles;
     private Camera currentCamera;
     private Vector2Int currentHover;
+    private Vector3 bounds;
 
     private void Awake()
     {
@@ -71,6 +74,10 @@ public class Chessboard : MonoBehaviour
     // Generates grid of chess board
     private void GenerateAllTiles(float tileSize, int tileCountX, int tileCountY)
     {
+        // To spawn pieces above and not inside of the board.
+        yOffset += transform.position.y;
+        bounds = new Vector3((tileCountX / 2) * tileSize, 0, (tileCountY / 2) * tileSize) + boardCenter;
+
         tiles = new GameObject[tileCountX, tileCountY];
         for (int x = 0; x < tileCountX; x++)
         {
@@ -94,10 +101,10 @@ public class Chessboard : MonoBehaviour
 
         // Generate the geometry
         Vector3[] vertices = new Vector3[4];
-        vertices[0] = new Vector3(x * tileSize, 0, y * tileSize);
-        vertices[1] = new Vector3(x * tileSize, 0, (y+1) * tileSize);
-        vertices[2] = new Vector3((x+1) * tileSize, 0, y * tileSize);
-        vertices[3] = new Vector3((x+1) * tileSize, 0, (y+1) * tileSize);
+        vertices[0] = new Vector3(x * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[1] = new Vector3(x * tileSize, yOffset, (y+1) * tileSize) - bounds;
+        vertices[2] = new Vector3((x+1) * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[3] = new Vector3((x+1) * tileSize, yOffset, (y+1) * tileSize) - bounds;
 
         // Triangle array to generate from vertices
         int[] tris = new int[] {0, 1, 2, 1, 3, 2 };
