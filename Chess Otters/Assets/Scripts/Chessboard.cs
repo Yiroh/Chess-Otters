c5491130bed717b9d5b0d91ceb0dbf569614fef3
Yiroh <69987911+Yiroh@users.noreply.github.com>
2024-02-07 15:35:54 -0500

    Added Victory King Otters
    Added Otter Kings based on which team won, added a 3d object to a canvas was a little difficult but easier now that I know.

--- Chess Otters/Assets/Scripts/Chessboard.cs
+++ Chess Otters/Assets/Scripts/Chessboard.cs
@@ -410,23 +410,19 @@ public class Chessboard : MonoBehaviour
             {
                 if(targetPawn.team == 0 && lastMove[1].y == 7)
                 {
-                    Debug.Log("Queen Spawning...");
                     ChessPiece newQueen = SpawnSinglePiece(ChessPieceType.Queen, 0);
                     newQueen.transform.position = chessPieces[lastMove[1].x, lastMove[1].y].transform.position;
                     Destroy(chessPieces[lastMove[1].x, lastMove[1].y].gameObject);
                     chessPieces[lastMove[1].x, lastMove[1].y] = newQueen;
                     PositionSinglePiece(lastMove[1].x, lastMove[1].y, true);
-                    Debug.Log("Queen Spawned for Blue Team...");
                 }
                 if(targetPawn.team == 1 && lastMove[1].y == 0)
                 {
-                    Debug.Log("Queen Spawning...");
                     ChessPiece newQueen = SpawnSinglePiece(ChessPieceType.Queen, 1);
                     newQueen.transform.position = chessPieces[lastMove[1].x, lastMove[1].y].transform.position;
                     Destroy(chessPieces[lastMove[1].x, lastMove[1].y].gameObject);
                     chessPieces[lastMove[1].x, lastMove[1].y] = newQueen;
                     PositionSinglePiece(lastMove[1].x, lastMove[1].y, true);
-                    Debug.Log("Queen Spawned for Red Team...");
                 }
             }
         }
